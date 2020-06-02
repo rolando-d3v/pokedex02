19c5d79da89935dccae5dede9e791a1ebfcc15ed
rolando-R <landon3388@gmail.com>
2020-06-01 22:52:27 -0500

    cambioa pokedex02

---
+++ src/pages/Buscar.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { Form, Button, Card } from "react-bootstrap";
+
+export default function Buscar() {
+  const [poke, setPoke] = useState({
+    pokemon: ""
+  });
+
+  const [total, setTotal] = useState([])
+
+  const { pokemon } = poke;
+
+  const LaApi = async () => {
+    let res = await fetch(`https://pokeapi.glitch.me/v1/pokemon/${pokemon}`);
+    let data = await res.json();
+    console.log(data);
+    setTotal(data);
+  };
+
+  const cargarDatos = (ev) => {
+    setPoke({
+      pokemon: ev.target.value,
+    });
+  };
+
+  const buscarPokemon = (ev) => {
+    ev.preventDefault();
+    if (!pokemon) {
+      alert("ingresa nombre de pokemon");
+    } else {
+      LaApi();
+    }
+  };
+
+  return (
+    <div>
+      <h3>Buscar Pokemon</h3>
+      <Form onSubmit={buscarPokemon}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Pokemon</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Nombre de Pokemon"
+            name="pokemon"
+            onChange={cargarDatos}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Buscar Pokemon
+        </Button>
+      </Form>
+
+      <div>
+        {total.map((ev)=> (
+          <Card key={ev.name}  style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={ev.sprite} />
+          <Card.Body>
+            <Card.Title> {ev.name} </Card.Title>
+             <Card.Text>{ev.description}</Card.Text>
+             <Card.Text>{ev.family.evolutionLine}</Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        ))}
+      </div>
+
+      
+    </div>
+  );
+}
+
+
+
+
+////
+
+// import React, { Component } from "react";
+// import { Form, Button } from "react-bootstrap";
+
+// export default class Buscar extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: "",
+//       img: "#",
+//     };
+//   }
+
+//   LaApi = async () => {
+//     let res = await fetch(`https://pokeapi.glitch.me/v1/pokemon/${this.state.name}`);
+//     let data = await res.json();
+//     console.log(data)
+//     this.setState({
+//         img: data.sprite
+//     })
+//   };
+
+//   cargarDatos = ev => {
+//       this.setState({
+//           name: ev.target.value
+//       })
+//   }
+
+//   buscarPokemon = ev => {
+//       ev.preventDefault()
+//       const poke = this.state.name
+//       console.log(poke);
+//       this.LaApi()
+
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h3>Buscar Pokemon</h3>
+//         <Form onSubmit={this.buscarPokemon} >
+//           <Form.Group controlId="formBasicEmail">
+//             <Form.Label>Pokemon</Form.Label>
+//             <Form.Control type="text" placeholder="Nombre de Pokemon"
+//             value={this.state.name}
+//             onChange={this.cargarDatos}
+//             />
+//           </Form.Group>
+
+//           <Button variant="primary" type="submit">
+//             Buscar Pokemon
+//           </Button>
+//         </Form>
+
+//         <img src={this.state.img} alt=""/>
+//       </div>
+//     );
+//   }
+// }
